670168bb287689badcb8e84f2ea3da9ca7495546
Gonzalo Ruiz de Villa <gonzalo.ruizdevilla@adesis.com>
2013-10-03 00:21:30 +0200

    chore(sanitize) add dependency of angular-sanitize
    Signed-off-by: Gonzalo Ruiz de Villa <gonzalo.ruizdevilla@adesis.com>

--- karma.conf.js
+++ karma.conf.js
@@ -14,6 +14,7 @@ module.exports = function(config) {
       'app/bower_components/angular/angular.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
       'app/bower_components/angular-route/angular-route.js',
+      'app/bower_components/angular-sanitize/angular-sanitize.js',
       'app/scripts/*.js',
       'app/scripts/**/*.js',
       'test/mock/**/*.js',
