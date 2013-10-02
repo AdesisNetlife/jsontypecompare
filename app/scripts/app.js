d5ddea813d41256d5c32985cc130adef3266202c
Gonzalo Ruiz de Villa <gonzalo.ruizdevilla@adesis.com>
2013-10-02 21:47:51 +0200

    chore(angular) updated dependencies to angular ~1.2.0

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('JsontypecompareApp', [])
+angular.module('JsontypecompareApp', ['ngRoute'])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
