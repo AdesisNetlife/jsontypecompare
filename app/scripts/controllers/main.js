664ddfdee1dd27ae3fdad66df882dd9ec79c851a
Gonzalo Ruiz de Villa <gonzalo.ruizdevilla@adesis.com>
2013-10-03 22:23:39 +0200

    feat(comparisonview) validate and prettify input

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,9 +3,50 @@
     'use strict';
 
     angular.module('JsontypecompareApp').controller('MainCtrl', function ($scope, ComparatorSrv) {
+        var o1, o2;
+        $scope.pretty = true;
+        $scope.validDocuments = false;
+        $scope.validLeftDocument = false;
+        $scope.validRightDocument = false;
+
+        $scope.prettifyLeft = function () {
+            if ($scope.pretty && o1) {
+                $scope.leftValue = JSON.stringify(o1, true, "  ");
+            }
+        };
+
+        $scope.prettifyRight = function () {
+            if ($scope.pretty && o1) {
+                $scope.leftValue = JSON.stringify(o1, true, "  ");
+            }
+        };
+
+        $scope.$watch("leftValue", function (newValue) {
+            o1 = null;
+            $scope.validLeftDocument = false;
+            if (newValue) {
+                try {
+                    o1 = JSON.parse(newValue);
+                    $scope.validLeftDocument = true;
+                } catch (ex) {
+                    $scope.validLeftDocument = false;
+                }
+                $scope.validDocuments = $scope.validLeftDocument && $scope.validRightDocument;
+            }
+        });
+        $scope.$watch("rightValue", function (newValue) {
+            $scope.validRightDocument = false;
+            if (newValue) {
+                try {
+                    o2 = JSON.parse(newValue);
+                    $scope.validRightDocument = true;
+                } catch (ex) {
+                    $scope.validRightDocument = false;
+                }
+                $scope.validDocuments = $scope.validLeftDocument && $scope.validRightDocument;
+            }
+        });
         $scope.compare = function () {
-            var o1 = JSON.parse($scope.leftValue),
-                o2 = JSON.parse($scope.rightValue);
             $scope.comparison = ComparatorSrv.compare(o1, o2);
         };
     });
