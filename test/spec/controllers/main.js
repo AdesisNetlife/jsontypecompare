6c26ac9749cb78908c11bd8ef3ad72f3aead8df1
Gonzalo Ruiz de Villa <gonzalo.ruizdevilla@adesis.com>
2013-10-05 12:21:57 +0200

    test(mainjs) missing test for view behaviour

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -17,9 +17,23 @@
             });
         }));
 
+        it('should inform when valid documents have been introducec', function () {
+            expect(scope.validDocuments).toBe(false);
+            scope.leftValue = '{"a": 1}';
+            scope.$digest();
+            expect(scope.validDocuments).toBe(false);
+            scope.rightValue = '{"a": 1';
+            scope.$digest();
+            expect(scope.validDocuments).toBe(false);
+            scope.rightValue = '{"a": 1}';
+            scope.$digest();
+            expect(scope.validDocuments).toBe(true);
+        });
+
         it('should compare json values', function () {
             scope.leftValue = '{"a": 1}';
             scope.rightValue = '{"a": 2}';
+            scope.$digest();
             scope.compare();
             expect(scope.comparison).toEqual({
                 properties: {
@@ -30,6 +44,24 @@
                 },
                 equal: true
             });
+        });
+        it('should generate report of differences', function () {
+            scope.leftValue = '{"a": {"b": 1}}';
+            scope.rightValue = '{"a": {"b": "2"}}';
+            scope.$digest();
+            scope.compare();
+            expect(scope.differences).toEqual([
+                {
+                    path: 'a',
+                    errorType: 'children'
+                },
+                {
+                    path: 'a.b',
+                    errorType: 'type',
+                    leftType: 'number',
+                    rightType: 'string'
+                }
+            ]);
         });
     });
 
